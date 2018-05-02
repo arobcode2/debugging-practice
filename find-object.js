a8841b559945384ac37b8a5f225e66f736307ae4
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 11:57:04 -0500

    I have found the place where the error might be and have placed a comment in the code

--- find-object.js
+++ find-object.js
@@ -15,9 +15,17 @@
 
 var findObject = function(list, key, value) {
   for (var i = 0; i < list.length; i++) {
-    if (list[i].key === value) {
+    if (list[i].key === value) {//The issue is probably the if statement, I expect it to find the value of breed which is 'Husky' and it should match the passed in value which is 'Husky'. Should return the first object in array.
       return list[i];
     }
   }
   return null;
 };
+
+var dogs = [
+    { name: 'Scout', breed: 'Husky' },
+    { name: 'Lucky', breed: 'Beagle' },
+    { name: 'Shadow', breed: 'Husky' },
+    { name: 'Molly', breed: 'Collie' }
+  ];
+console.log(findObject(dogs, 'breed', 'Husky'));
