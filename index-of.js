69b016cc5d052006818b9d22bbf535a8419be45f
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 12:23:20 -0500

    Fixed the bug based on my understanding of the problem

--- index-of.js
+++ index-of.js
@@ -5,15 +5,17 @@
 */
 
 var indexOf = function(arr, val) {
-  let found = [];
+  //let found = [];
+  //console.log(found);
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
-      found.push(i);
-    } else {//I believe the issue is the else statement. When looping and it checks the first element in arr for val and doesn't find it, it would return -1 immediately. However, the target val could be in the arr, so it needs to loop through every value and when it reaches the end of the arr and the target val is still not found, then it should return -1.
-      return -1;
+      return i;
     }
+    // } else {//I believe the issue is the else statement. When looping and it checks the first element in arr for val and doesn't find it, it would return -1 immediately. However, the target val could be in the arr, so it needs to loop through every value and when it reaches the end of the arr and the target val is still not found, then it should return -1.
+    //   console.log('I am in the else');
+    //   return -1;
+    // }
   }
-  return found[found.length - 1];
+  return -1;
+  //return found[found.length - 1];
 };
-
-console.log(indexOf(['a', 'b', 'c'], 'b'));
