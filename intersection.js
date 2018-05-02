0046d03893aeb0e7e64e1d748bd24ae71ae9d242
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 12:30:28 -0500

    Have found a place where the bug might be

--- intersection.js
+++ intersection.js
@@ -13,10 +13,14 @@ var intersection = function(arrA, arrB) {
   var newArr = [];
   for (var i = 0; i < arrA.length; i++) {
     for (var j = 0; j < arrB.length; j++) {
-      if (arrA[i] === arrB[j]) {
+      if (arrA[i] === arrB[j]) {//I believe the bug is the if statement comparison. In this case (example) the arrays are not the same length and comparing each element in both arrays at the same index(s) will lead to missing shared elements
         newArr.push(i);
       }
     }
   }
   return newArr;
 };
+
+var a = [4, 1, 2, 3];
+var b = [3, 7, 5, 1, 9, 8];
+console.log(intersection(a, b));
