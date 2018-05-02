44fdf90e59709a6ba292971ad1e58ddb12c6bdbc
Andrew Rob <andrew-c-rob@live.com>
2018-05-02 12:44:24 -0500

    Have fixed the bug, it was on line 17 where instead of pushing the index of the shared element into newArr, it should have pushed the shared element into newArr

--- intersection.js
+++ intersection.js
@@ -13,14 +13,10 @@ var intersection = function(arrA, arrB) {
   var newArr = [];
   for (var i = 0; i < arrA.length; i++) {
     for (var j = 0; j < arrB.length; j++) {
-      if (arrA[i] === arrB[j]) {//I believe the bug is the if statement comparison. In this case (example) the arrays are not the same length and comparing each element in both arrays at the same index(s) will lead to missing shared elements
-        newArr.push(i);
+      if (arrA[i] === arrB[j]) {
+        newArr.push(arrA[i]);
       }
     }
   }
   return newArr;
 };
-
-var a = [4, 1, 2, 3];
-var b = [3, 7, 5, 1, 9, 8];
-console.log(intersection(a, b));
